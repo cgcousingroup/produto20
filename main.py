ca4191906db48cebeadf1db29f2f61f7694ac349
cgcousingroup <cgcousingroup@gmail.com>
2025-07-11 16:38:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ GRUPOS_MONITORADOS = {
 produtos = [
     (
         "🔞 VIP Proibidão 🔞",
-        "ANTES: R$19,90 | AGORA: R$9,90",
+        "De R$19,90 | Por R$9,90",
         "https://app.pushinpay.com.br/service/pay/9F484D59-0B3F-4BA0-A819-4C6B8396684C",
         "https://app.pushinpay.com.br/service/pay/9F484D59-0B3F-4BA0-A819-4C6B8396684C",
         "https://www.eutesalvo.com/arquivos/upload/eutesalvo-2885119e48e33dd3348cf5949b7a57e0.jpg"
